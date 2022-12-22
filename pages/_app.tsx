ac5a42e3ae356678ce26f84edd2529c8303bbe8a
guyuri <yulrhymz@gmail.com>
2022-12-22 16:00:51 +0900

    used @tanstack/react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app';
 import AppLayout from '@components/AppLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { staleTime: Infinity },
+    },
+  });
+
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <QueryClientProvider client={queryClient}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </QueryClientProvider>
   );
 }
