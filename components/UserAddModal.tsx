7d792bbaf8940756720e98122d8b95bf76df133b
guyuri <yulrhymz@gmail.com>
2023-01-05 17:58:26 +0900

    filled contents to add user in modal

--- components/UserAddModal.tsx
+++ components/UserAddModal.tsx
@@ -1,7 +1,23 @@
 import { useContext } from 'react';
-import { Modal, Form } from 'antd';
+import {
+  Modal,
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+  Checkbox,
+  Upload,
+} from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import { AppContext } from 'context/context';
-
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
 const UserAddModal = () => {
   const { isModalOpen, setIsModalOpen } = useContext(AppContext);
 
@@ -16,7 +32,47 @@ const UserAddModal = () => {
         setIsModalOpen(false);
       }}
     >
-      <p>Some contents...</p>
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        // onValuesChange={onFormLayoutChange}
+      >
+        {/* <Form.Item label="Checkbox" name="disabled" valuePropName="checked">
+          <Checkbox>Checkbox</Checkbox>
+        </Form.Item> */}
+        {/* <Form.Item label="Radio">
+          <Radio.Group>
+            <Radio value="apple"> Apple </Radio>
+            <Radio value="pear"> Pear </Radio>
+          </Radio.Group>
+        </Form.Item> */}
+        <Form.Item label="Name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Select Group">
+          <Select>
+            <Select.Option value="Default">Default</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="Birthday">
+          <DatePicker />
+        </Form.Item>
+        <Form.Item label="Description">
+          <TextArea rows={4} />
+        </Form.Item>
+        <Form.Item label="Upload" valuePropName="fileList">
+          <Upload action="/upload.do" listType="picture-card">
+            <div>
+              <PlusOutlined />
+              <div style={{ marginTop: 8 }}>Upload</div>
+            </div>
+          </Upload>
+        </Form.Item>
+        <Form.Item label="Button">
+          <Button>Button</Button>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
